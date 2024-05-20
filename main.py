5a198030fccc218c196cd8acbdbf7d8fca41a3ae
juhyun721 <juhyun721@gnu.ac.kr>
2024-05-20 14:15:43 +0900

    Revert "새로운 fastapi 프로젝트"
    This reverts commit e0bff106e59cd0669cc0e3dbe040a98221880448.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from typing import Union
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
@@ -13,4 +14,4 @@ templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
 async def root(request: Request): # HTML return
-    return templates.TemplateResponse(request=request, name="main.html")
+    return templates. TemplateResponse(request=request, name="main.html")
