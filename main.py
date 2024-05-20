e0bff106e59cd0669cc0e3dbe040a98221880448
juhyun721 <juhyun721@gnu.ac.kr>
2024-05-20 14:13:01 +0900

    새로운 fastapi 프로젝트

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from typing import Union
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
@@ -14,4 +13,4 @@ templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
 async def root(request: Request): # HTML return
-    return templates. TemplateResponse(request=request, name="main.html")
+    return templates.TemplateResponse(request=request, name="main.html")
